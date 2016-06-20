89939cb8c400e1c65005884729883bddcdbb502b
Rémy Kaloustian <remy.kaloustian@gmail.com>
2016-06-20 09:14:32 +0200

    fix #4

---
+++ Notes/Notes/assets/js/main.js
@@ -0,0 +1,17 @@
+﻿$(function () { //DOM Ready 
+
+
+    notie.input({
+        type: 'email',
+        placeholder: 'name@example.com',
+    prefilledValue: 'jane@doe.com'
+}, 'Please enter your email address:', 'Submit', 'Cancel', 'email', 'name@example.com', function(valueEntered) {
+    notie.alert(1, 'You entered: ' + valueEntered, 2);
+});
+
+    //$(".gridster ul").gridster({
+    //    widget_margins: [0, 0],
+    //    widget_base_dimensions: [0, 0]
+    //});
+
+});
--- Notes/Notes/main.js
+++
@@ -1,8 +0,0 @@
-﻿$(function () { //DOM Ready 
-
-    $(".gridster ul").gridster({
-        widget_margins: [0, 0],
-        widget_base_dimensions: [0, 0]
-    });
-
-});
